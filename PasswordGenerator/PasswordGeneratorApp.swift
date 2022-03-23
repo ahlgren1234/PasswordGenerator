348588f841784d27a4ea7f26396e9980344d3ba1
Peter Ahlgren <ahlgren1234@gmail.com>
2022-03-23 12:12:17 +0100

    Initial Commit

---
+++ PasswordGenerator/PasswordGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordGeneratorApp.swift
+//  PasswordGenerator
+//
+//  Created by Peter Ahlgren on 2022-03-23.
+//
+
+import SwiftUI
+
+@main
+struct PasswordGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
